b28327bb3d096ef2eac7f4aeeba2898dfc746c3a
@eyob_birhanu <eyob@kemercode.com>
2023-08-05 15:36:51 +0300

    ✨ feat(routers): entry point
    add suer route in the entry point app.ts

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,16 +1,24 @@
-import express, { Application, Request, Response } from 'express';
-import entitiesRouter from './clean_me.js';  // import entities router
+import express, { Application, Request, Response } from "express";
+import entitiesRouter from "./clean_me.js"; // import entities router
 
 const app: Application = express();
 
 const PORT: number = 3001;
 
-app.use('/', entitiesRouter);  // use entities router at path '/entities'
+//Load Routes
+import userRoute from "./routes/users";
+// Use Routes
+const apiVersion = "/api/v1";
 
-app.use('/', (req: Request, res: Response): void => {
-    res.send('Hello world!');
+//use routes
+app.use(`${apiVersion}/auth`, userRoute);
+
+app.use("/", entitiesRouter); // use entities router at path '/entities'
+
+app.use("/", (req: Request, res: Response): void => {
+  res.send("Hello world!");
 });
 
 app.listen(PORT, (): void => {
-    console.log('SERVER IS UP ON PORT:', PORT);
+  console.log("SERVER IS UP ON PORT:", PORT);
 });
