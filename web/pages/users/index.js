028501aff6e933c65dbbb5f053e0ab0414c9794c
@eyob_birhanu <eyob@kemercode.com>
2023-08-05 16:10:36 +0300

    ✨ feat(users): get all users

--- web/pages/users/index.js
+++ web/pages/users/index.js
@@ -1,1 +1,44 @@
 //DO THE USERS PAGE HERE AND THE MODAL TO ADD NEW USER ALSO HERE
+import React, { useState, userEffect } from "react";
+import UserTable from "../../components/UserTable";
+import AddUserModal from "../../components/AddUserModal";
+import { getUsers } from "../utils/api";
+
+const Home = () => {
+  const [users, setUsers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    getUsers()
+      .then((data) => setUsers(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleAddUser = (newUser) => {
+    //Todo
+    setUsers((prevUsers) => [...prevUsers, newUser]);
+  };
+
+  return (
+    <div className="container mx-auto mt-8">
+      <h1 className="text-3xl font-bold mb-4">User Table</h1>
+      <button
+        onClick={() => setIsModalOpen(true)}
+        className="px-4 py-2 bg-blue-500 text-white rounded"
+      >
+        Add User
+      </button>
+      <UserTable users={users} />
+
+      {isModalOpen && (
+        <AddUserModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          onAddUser={handleAddUser}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Home;
